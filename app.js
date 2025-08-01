071580587ece1a184ad721d405dd6a486b0459f1
ProfeAlons <eabj2723@gmail.com>
2025-07-31 19:25:08 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+let amigos = [];
+
+/**
+ * Agrega un nuevo amigo al arreglo de amigos.
+ * El nombre no está vacío antes de agregarlo y actualiza la lista.
+ */
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+
+  if (nombreAmigo.trim() === "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    amigos.push(nombreAmigo);
+    document.querySelector("#amigo").value = "";
+    mostrarListaAmigo();
+  }
+}
+
+/**
+ * Actualiza la visualización de la lista, creando elementos <li> para cada amigo.
+ */
+function mostrarListaAmigo() {
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let index = 0; index < amigos.length; index++) {
+    const element = amigos[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+    listaAmigos.appendChild(listaHTML);
+  }
+}
+
+/**
+ * Sortea y muestra un miembro de la lista de amigos aleatoriamente.
+ * La lista no está vacía antes del sorteo.
+ */
+function sortearAmigo() {
+  let cantidadAmigos = amigos.length;
+  if (cantidadAmigos === 0) {
+    alert("Por favor, inserte un nombre antes de sortear");
+  } else {
+    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    let resultadoHTML = document.querySelector("#resultado");
+    resultadoHTML.innerHTML = amigos[indiceAmigo];
+  }
+}
